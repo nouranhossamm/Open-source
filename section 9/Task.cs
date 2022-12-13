e849a2d5b9def35a133e2ca24ede31ee7ae1549c
Nouran Hossameldin <111601276+nouranhossamm@users.noreply.github.com>
2022-12-13 20:01:31 +0200

    Add files via upload

---
+++ section 9/Task.cs
@@ -0,0 +1,30 @@
+using System; //Directive: include sdk related types
+namespace Task;  //creating new namespace called 'Task'
+
+public class Task
+{
+  private static void Main()
+  {
+    
+    Console.Write("please enter your name: ");
+   /* var name = Console.ReadLine();
+    Console.WriteLine("Hello" + name);  //public  static method within the public static class 'console'
+    Console.WriteLine("Hello, World") ; //public  static method within the public static class 'console'
+
+    byte i0 = 100 ;// 0 ,  255
+    sbyte i1 = 100; //-128,  127
+    short i2 = 30_000; // -32_000 , 32_000 
+    int i3 = -2_000_000_000; // 
+
+
+
+    char c = 'a';
+    bool b = true;
+
+
+
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.Write(number * number);
+    */
+  }  
+};
