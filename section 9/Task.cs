a0bc83399ccd017ca788a73b8dfa11ecb533a7e4
Nouran Hossameldin <111601276+nouranhossamm@users.noreply.github.com>
2022-12-13 21:14:34 +0200

    Update Task.cs

--- section 9/Task.cs
+++ section 9/Task.cs
@@ -32,12 +32,18 @@ public float Gpa;
 public class Database{
   
   int _currentIndex;
+  int _stuffMember;
 
   public Person[] People = new Person[50];
+  public Staff[] Staff = new Staff[50];
 
   public void AddStudent(Student student){
     // if( _currentIndex == 49) return;
     People[_currentIndex++] = student;
+  } 
+
+  public void AddStaff(Staff staff){
+    Staff[_stuffMember++] = staff;
   } 
 }
 
@@ -63,6 +69,7 @@ public class Task
   private static void Main()
   {
     // Console.Write("Hello, World");
+    // student
       var database = new Database();
       
       Console.Write("Name: ");
@@ -75,8 +82,22 @@ public class Task
       var gpa = Convert.ToSingle(Console.ReadLine());
       
       var student = new Student(name, age, year, gpa);
+    // staff
+      Console.Write("Name: ");
+      var name1 = Console.ReadLine();
+      Console.Write("Age: ");
+      var age1 = Convert.ToInt32(Console.ReadLine());
+      Console.Write("Salary: ");
+      var salary = Convert.ToDouble(Console.ReadLine());
+      Console.Write("JoinYear: ");
+      var joinYear = Convert.ToInt32(Console.ReadLine());
+
+      var staff = new Staff(name1, age1, salary, joinYear);
+
+
 
       database.AddStudent(student);
+      database.AddStaff(staff);
   
   }  
 };
